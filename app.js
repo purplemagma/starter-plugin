147ec20efb03d99439ee0717bb94aeb49a242114
Jeff Brewer <jeff@purplemagma.com>
2014-12-11 00:24:13 -0800

    Updated to express v4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var port = 34212;
 var localServerName = "localhost.intuit.com:"+port;
 
 var express = require("express");
+var morgan = require("morgan");
 var https = require("https");
 var path = require("path");
 var fs = require("fs");
@@ -11,13 +12,19 @@ var app = express();
 
 // all environments
 app.set("port", process.env.PORT || port);
-app.use(express.logger());
+app.use(morgan('combined'));
 app.use(cors());
 app.options("*", cors());
 app.use("/", express.static(__dirname+"/src"));
 
 if ("development" == app.get("env")) {
-  app.use(express.errorHandler());
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
 var privateKey = fs.readFileSync("sslcert/server.key");
